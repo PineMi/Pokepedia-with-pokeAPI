8e56eacfdca18d262aa9104850db371024710a6c
Miguel Piñeiro <129214520+PineMi@users.noreply.github.com>
2024-02-14 23:59:39 -0300

    Created the structure to visualize additional info and added the css

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // PokeAPI GET fetch
+let currentExpandedCard = null;
 
 fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
     .then(response => response.json())
@@ -6,19 +7,51 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
         
         
         data.results.forEach(pokemon => {
-
-            const pokemonCard = document.createElement('div');
             let pokeIDRaw = pokemon.url;
-            pokeID = pokeIDRaw.slice(34, pokeIDRaw.lenght).replace(/\D/g, "");
+            let pokeID = pokeIDRaw.slice(34, pokeIDRaw.lenght).replace(/\D/g, "");
             console.log(pokeID)
 
+
+            //Link to the png of the pokemons
+            //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png
+            
             // Structure of the "cards" 
+            const pokemonCard = document.createElement('div');
+            pokemonCard.classList.add('pokemon-container')
+            pokemonCard.setAttribute("onclick", "togglePokemonInfo(this)")
+            
             pokemonCard.innerHTML = `
-                <p>${pokemon.name}</p>
-                <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png'></img>                
+                <p class="pokemon-name">${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</p>
+                <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${pokeID}.gif' alt="Pokemon Gif" class="pokemon-photo"></img>
+                <div class="additional-info">    
+                </div>                
+                <p class="identification-paragraph">${pokeID}</p>
             `
+            
+            pokemonCard.id = `card${pokeID}`
             document.body.appendChild(pokemonCard)
 
         });
     })
 
+function togglePokemonInfo(element) {
+    if (currentExpandedCard && currentExpandedCard !== element) {
+        // Close the previously expanded card
+        currentExpandedCard.classList.remove('expanded');
+        let prevAdditionalInfo = currentExpandedCard.querySelector('.additional-info');
+        if (prevAdditionalInfo) {
+          prevAdditionalInfo.classList.remove('show');
+        }
+      }
+    
+      element.classList.toggle('expanded');
+    
+      // Find the .additional-info element within the clicked container
+      var additionalInfo = element.querySelector('.additional-info');
+    
+      // Here, all the information about the pokemon will be added
+      additionalInfo.innerHTML = ``
+
+      additionalInfo.classList.toggle('show');
+      currentExpandedCard = element;
+}
